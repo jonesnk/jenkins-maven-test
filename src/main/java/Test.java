9a765cea4fc1cd26a66aef0f303fab42f0d509f9
bgi056 <bodymoves@gmail.com>
2016-07-13 19:56:21 -0400

    updated pom

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,9 +1,13 @@
+import org.apache.log4j.Logger;
 
 public class Test {
 
+
+    private static final Logger logger = Logger.getLogger(Test.class);
+
     public static void main (String[] args){
 
-        System.out.println("Hello Jenkins Maven!");
+        System.out.println("Hello Jenkins Maven, it's Nicole!");
 
     }
 }
