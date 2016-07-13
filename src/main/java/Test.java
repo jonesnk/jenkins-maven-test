f72237949254562daca8a479fc1414353e3e24ce
bgi056 <bodymoves@gmail.com>
2016-07-13 18:58:34 -0400

    adding jenkins maven test

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,10 @@
-/**
- * Created by bgi056 on 7/13/16.
- */
+
 public class Test {
+
+    public static void main (String[] args){
+
+
+        System.out.println("Hello Jenkins Maven!");
+
+    }
 }
