38bf7e2dc545015fc6a168d15cae0efbfc4874e6
bgi056 <bodymoves@gmail.com>
2016-07-14 09:49:19 -0400

    fixed untracked files

---
+++ src/main/java/org/jonesnk/com/driver/Test.java
@@ -0,0 +1,12 @@
+package org.jonesnk.com.driver;
+
+public class Test {
+
+    private Test(){}
+
+    public static void main (String[] args){
+
+        System.out.println("Hello Jenkins Maven, it's Nicole!");
+
+    }
+}
