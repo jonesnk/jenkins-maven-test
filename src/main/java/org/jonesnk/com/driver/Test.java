9f4cb8db6a5aab6344ea28a726ed106193442c03
bgi056 <bodymoves@gmail.com>
2016-07-14 09:59:26 -0400

    added logger to Test class

--- src/main/java/org/jonesnk/com/driver/Test.java
+++ src/main/java/org/jonesnk/com/driver/Test.java
@@ -4,6 +4,8 @@ import org.apache.log4j.Logger;
 
 public class Test {
 
+    private Test(){}
+
     private static final Logger logger = Logger.getLogger(Test.class);
 
     public static void main (String[] args){
