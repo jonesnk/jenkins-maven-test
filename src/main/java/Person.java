92f2f5e26c2b7369ab12c5ac830143ca93d5ebc8
bgi056 <bodymoves@gmail.com>
2016-07-14 08:53:23 -0400

    added Person class

---
+++ src/main/java/Person.java
@@ -0,0 +1,40 @@
+
+public class Person {
+
+    private String name;
+    private Integer age;
+    private String occupation;
+
+    public Person(){}
+
+    public Person(String name, Integer age, String occupation) {
+        this.name = name;
+        this.age = age;
+        this.occupation = occupation;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+}
