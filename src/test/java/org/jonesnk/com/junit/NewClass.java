f1df1d0b09128708d060ddfca553015cf5007287
jonesnk <nicole.k.jones@gmail.com>
2016-07-17 22:08:48 -0400

    added profiles

---
+++ src/test/java/org/jonesnk/com/junit/NewClass.java
@@ -0,0 +1,26 @@
+package org.jonesnk.com.junit;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by bgi056 on 7/16/16.
+ */
+public class NewClass {
+
+
+    private WebDriver driver;
+    @Test
+    public void testEasy() {
+        driver.get("http://www.guru99.com/selenium-tutorial.html");
+        String title = driver.getTitle();
+        Assert.assertTrue(title.contains("Free Selenium Tutorials"));
+    }
+
+
+
+
+
+}
