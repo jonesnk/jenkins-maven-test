83a7fa3975d558bc6f75ee2950cbb0683b768333
jonesnk <nicole.k.jones@gmail.com>
2016-08-20 17:38:13 -0400

    testing again

--- src/main/java/org/jonesnk/com/driver/Test.java
+++ src/main/java/org/jonesnk/com/driver/Test.java
@@ -12,7 +12,7 @@ public class Test {
 
     public static void main (String[] args){
 
-        logger.info("Hello Jenkins Maven, it's Nicole!");
+        logger.info("Hello Jenkins Maven, it's Nicole! Testing this thing again!!!");
 
     }
 }
